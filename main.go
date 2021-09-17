2f237c28b9f598597a87578d9b7cb5e7474c30f7
SithumDev07 <sithum.dashantha@gmail.com>
2021-09-18 00:03:01 +0530

    Responding to requests

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main () {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
 		log.Println("Hello World")
+		d, _ := ioutil.ReadAll(r.Body)
 
+		fmt.Fprintf(rw, "Hello %s", d)
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
