8e0edb5e99f8014e1e9e1736b6a58a1350da4e4c
SithumDev07 <sithum.dashantha@gmail.com>
2021-09-18 09:21:18 +0530

    File Structure

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ import (
 func main () {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	// Handlers
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
 	http.ListenAndServe(":8081", sm)
 }
