939c825ba60254265d85d4e66a87c5336ba49844
SithumDev07 <sithum.dashantha@gmail.com>
2021-09-18 12:20:48 +0530

    POST request

--- handlers/products.go
+++ handlers/products.go
@@ -22,15 +22,34 @@ func (p*Products) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	if req.Method == http.MethodPost {
+		p.addProduct(rw, req)
+		return
+	}
+
 	// catch All
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+
+// * GET
 func (p *Products) getProducts(rw http.ResponseWriter, req *http.Request) {
 	productList := data.GetProducts()
 	err := productList.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+
+// * POST
+func (p *Products) addProduct(rw http.ResponseWriter, req *http.Request) {
+	product := &data.Product{}
+	err := product.FromJSON(req.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Product: %#v", product)
 }
